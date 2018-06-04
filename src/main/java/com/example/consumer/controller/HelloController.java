32ca21f3c023e197da378ed6b1b8e5fbd399f734
Mr2277 <837544713@qq.com>
2018-06-04 15:18:00 +0800

    init

--- src/main/java/com/example/consumer/controller/HelloController.java
+++ src/main/java/com/example/consumer/controller/HelloController.java
@@ -1,7 +1,19 @@
 package com.example.consumer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        System.out.println("ffffffffffffffffffffffffffff");
+        return restTemplate.getForEntity("http://SERVICE-HELLO/hello", String.class).getBody();
+    }
 }
